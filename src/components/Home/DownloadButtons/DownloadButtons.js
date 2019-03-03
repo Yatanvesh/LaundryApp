c77a897e6b5fcdb98baf5c3fd34505484b779cb3
Yatan Vesh <yatan.vesh@gmail.com>
2019-03-03 17:43:13 +0530

    landing page finished

---
+++ src/components/Home/DownloadButtons/DownloadButtons.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Row from "../Home/Home";
+import {Link} from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faApple, faGooglePlay} from "@fortawesome/free-brands-svg-icons";
+
+import Col from 'react-bootstrap/Col';
+const DownloadButtons = () =>  {
+    return (
+        <div>
+            <Row>
+                <Col className='text-center mt-4'>
+                    <Link to={'/book'} className="btn btn-danger btn-xl text-light px-3 "> <span
+                        className="btn-text">Book Now</span></Link>
+                </Col>
+            </Row>
+            <Row>
+                <Col className='text-center'>
+                    <button className="btn btn-dark btn-lg app-download-button mx-2" type="button">
+                        <FontAwesomeIcon icon={faApple}/>
+                        <span className='pl-1'>Download</span>
+                    </button>
+
+                    <button className="btn btn-outline-light btn-lg app-download-button mx-2" type="button">
+                        <FontAwesomeIcon icon={faGooglePlay}/>
+                        <span className='pl-1'>Download</span>
+                    </button>
+                </Col>
+            </Row>
+        </div>
+    );
+};
+
+export default DownloadButtons;
