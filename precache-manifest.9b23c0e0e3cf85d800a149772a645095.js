736e23bbc90bace384e38f7d9eb4be13f35c62b1
Yatan Vesh <yatan.vesh@gmail.com>
2019-03-12 16:01:16 +0530

    Updates

---
+++ precache-manifest.9b23c0e0e3cf85d800a149772a645095.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b865c915fc4dc46f3232",
+    "url": "/laundry/static/css/main.0d64c247.chunk.css"
+  },
+  {
+    "revision": "b865c915fc4dc46f3232",
+    "url": "/laundry/static/js/main.b865c915.chunk.js"
+  },
+  {
+    "revision": "75e061e3ae65335acb18",
+    "url": "/laundry/static/js/runtime~main.75e061e3.js"
+  },
+  {
+    "revision": "9a51769a6110e4622da8",
+    "url": "/laundry/static/css/2.257a6eec.chunk.css"
+  },
+  {
+    "revision": "9a51769a6110e4622da8",
+    "url": "/laundry/static/js/2.9a51769a.chunk.js"
+  },
+  {
+    "revision": "5064273120cce3b5da912c019448ae98",
+    "url": "/laundry/static/media/funy2.50642731.jpeg"
+  },
+  {
+    "revision": "7bb02a300e1f78df482cc43565deb601",
+    "url": "/laundry/static/media/face3.7bb02a30.jpg"
+  },
+  {
+    "revision": "ce07a5a12953842072f015742eac70d0",
+    "url": "/laundry/index.html"
+  }
+];
